cedcc3b01398d5858df16914489652ba552ee560
Nguyen van Phong <ngvanphong2012@gmail.com>
2023-12-03 08:08:32 +0700

    52522

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,41 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-using System;
+#region
 
-int a = 35;
-int b = 20;
-int tong = a + b;
-int nhan = a * b;
-int chia = a / b;
-int phandu= a % b;
+//// See https://aka.ms/new-console-template for more information
 
-bool dungsai= a>b; // true
-dungsai = a > b && a < 20; //false
-dungsai = a >= b || a < 20 || a<=30; //true;
-dungsai = a == b || a != 35;// false;
-dungsai= a>b && (a>b || a <20); // true;
+//using System;
 
-dungsai = !(a >= b); /// false;
-//Console.WriteLine($"Tong = {tong}");
+//int a = 35;
+//int b = 20;
+//int tong = a + b;
+//int nhan = a * b;
+//int chia = a / b;
+//int phandu= a % b;
 
-int c = 30;
-//c++; // 31 c= c+1;
-//c--; //30;
-//c += 2; // 32; c= c+2;
-//c -= 2;
-int k = c++;
-int g = k += 1;
-//int k = ++c;
+//bool dungsai= a>b; // true
+//dungsai = a > b && a < 20; //false
+//dungsai = a >= b || a < 20 || a<=30; //true;
+//dungsai = a == b || a != 35;// false;
+//dungsai= a>b && (a>b || a <20); // true;
 
-int m= 3;
-int n = 10;
+//dungsai = !(a >= b); /// false;
+////Console.WriteLine($"Tong = {tong}");
 
-int o = !(m > n) ? (m+n): 300;
+//int c = 30;
+////c++; // 31 c= c+1;
+////c--; //30;
+////c += 2; // 32; c= c+2;
+////c -= 2;
+//int k = c++;
+//int g = k += 1;
+////int k = ++c;
+
+//int m= 3;
+//int n = 10;
+
+//int o = !(m > n) ? (m+n): 300;
 
 
-Console.WriteLine(o);
+//Console.WriteLine(o);
 
-
+#endregion
 
 
 
